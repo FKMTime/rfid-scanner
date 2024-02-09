1b98df2c2c617f6d6bb56f7323c3b0164609696e
filipton <filipton12@gmail.com>
2024-02-09 15:47:26 +0100

    feat: simple serial rfid

--- src/main.cpp
+++ src/main.cpp
@@ -8,61 +8,28 @@
 #define SCK_PIN 18
 
 MFRC522 mfrc522(CS_PIN, UNUSED_PIN);
-unsigned long lastCardId = 0;
-std::vector<unsigned long> lastCards;
-
 void setup() {
   Serial.begin(9600);
-  // SPI.begin(SCK_PIN, MISO_PIN, MOSI_PIN, CS_PIN);
   SPI.begin();
   mfrc522.PCD_Init();
 
   pinMode(LED_BUILTIN, OUTPUT);
 }
 
-unsigned long cardScanTimeout = 0;
+unsigned long blinkLedTimeout = 0;
 void loop() {
-  if (millis() - cardScanTimeout > 5000 && millis() - cardScanTimeout < 5500) {
+  if (millis() - blinkLedTimeout > 5000 && millis() - blinkLedTimeout < 5500) {
     digitalWrite(LED_BUILTIN, HIGH);
-  } else if(millis() - cardScanTimeout > 5500) {
+  } else if(millis() - blinkLedTimeout > 5500) {
     digitalWrite(LED_BUILTIN, LOW);
-    cardScanTimeout = millis();
+    blinkLedTimeout = millis();
   }
 
   if(mfrc522.PICC_IsNewCardPresent() && mfrc522.PICC_ReadCardSerial())
   {
     unsigned long cardId = mfrc522.uid.uidByte[0] + (mfrc522.uid.uidByte[1] << 8) + (mfrc522.uid.uidByte[2] << 16) + (mfrc522.uid.uidByte[3] << 24);
-    if (lastCardId != cardId) {
-      bool duplicated = false;
-      for (int i = 0; i < (int)lastCards.size(); i++) {
-        if (lastCards[i] == cardId) {
-          duplicated = true;
-          break;
-        }
-      }
+    Serial.println(cardId);
 
-      if (duplicated) {
-        Serial.print("DUPLICATE DETECTED!!!!! (");
-        Serial.print(cardId);
-        Serial.println(")");
-      } else {
-        lastCards.push_back(cardId);
-      }
-      /*
-      if (std::find(lastCards.begin(), lastCards.end(), cardId) != lastCards.end()) {
-        Serial.print("DUPLICATE DETECTED!!!!! (");
-        Serial.print(cardId);
-        Serial.println(")");
-      } else {
-        lastCards.push_back(cardId);
-      }
-      */
-
-      Serial.print("Card ID: ");
-      Serial.print(cardId);
-      Serial.print(" | COUNT: ");
-      Serial.println(lastCards.size());
-      lastCardId = cardId;
-    }
+    mfrc522.PICC_HaltA();
   }
 }
